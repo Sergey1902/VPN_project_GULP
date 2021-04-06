ada8f6c7c5428537eba7e1a3c99d8ae884fbf864
Sergey <sergeylawyer@mail.ru>
2021-04-06 20:42:12 +0300

    first commit

---
+++ build/js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+
+// Слайдер тренеров
+
+// (function () {
+//   var sectionTrainers = document.querySelector('.trainers');
+//   var sliderTrainers = sectionTrainers.querySelector('.slider');
+
+//   if (!sectionTrainers.classList.contains('only-js')) {
+//     sectionTrainers.classList.add('only-js');
+//   }
+
+//   if (sliderTrainers) {
+//     sliderTrainers = new window.Swiper(sliderTrainers, {
+//       slidesPerView: 4,
+//       slidesPerGroup: 4,
+//       spaceBetween: 40,
+//       slideClass: 'slider__item',
+//       wrapperClass: 'slider__list',
+//       navigation: {
+//         nextEl: '.trainers__button--next',
+//         prevEl: '.trainers__button--prev',
+//       },
+//       breakpoints: {
+//         320: {
+//           slidesPerView: 1,
+//           slidesPerGroup: 1,
+//           spaceBetween: 0
+//         },
+//         768: {
+//           slidesPerView: 2,
+//           slidesPerGroup: 2,
+//           spaceBetween: 30
+//         },
+//         1200: {
+//           slidesPerView: 3,
+//           slidesPerGroup: 3,
+//           spaceBetween: 40
+//         },
+//         1311: {
+//           slidesPerView: 4,
+//           slidesPerGroup: 4,
+//           spaceBetween: 40
+//         }
+//       }
+//     });
+//   }
+// })();
+
+// var mySwiper = new Swiper('.swiper-container', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 3,
+//   cssMode: true,
+
+//   breakpoints: {
+//       1025: {
+//           slidesPerView: 3,
+//       },
+
+//       769: {
+//           slidesPerView: 2,
+//       },
+
+//       350: {
+//           slidesPerView: 1,
+//       },
+//   },
+
+//   pagination: {
+//       el: '.swiper-pagination',
+//       dynamicBullets: true,
+//       clickable: true,
+//   },
+
+//   navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//   },
+// });
+
+// Слайдер отзывов
